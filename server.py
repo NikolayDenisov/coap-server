71035a167a43ee70b8a75d2eb57d3e97c844db0e
Николай Денисов <nickdenisov@protonmail.com>
2022-05-09 12:16:14 +0500

    Changed(server): check a devece token

--- server.py
+++ server.py
@@ -21,9 +21,6 @@ class ActiveThing(resource.Resource):
         return aiocoap.Message(code=aiocoap.CREATED,
                                payload=text.encode('utf8'))
 
-    @staticmethod
-    async def parse_payload(data):
-        printf(f"data {data}")
 
 
 
@@ -36,14 +33,16 @@ class ThingWrite(resource.Resource, resource.PathCapable):
 
     async def render_post(self, request):
         device_token = request.opt.uri_path[0]
+        if not device_token:
+            return aiocoap.Message(code=aiocoap.UNAUTHORIZED)
         dev_ip_addr = request.remote.sockaddr[0]
         uri_query = request.opt.uri_query
         logging.info(f'Receive token {device_token}, dev_ip_addr {dev_ip_addr}, uri_query {uri_query}')
         payload = request.payload.decode('utf8')
         logging.info(payload)
-        text = "Hello world"
-        return aiocoap.Message(content_format=0,
-                               payload=text.encode('utf8'))
+        return aiocoap.Message(code=aiocoap.CREATED,
+                               payload=payload.encode('utf8'))
+
 
 class ThingRead(resource.Resource):
     """
@@ -85,12 +84,12 @@ logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 async def main():
     # Resource tree creation
-    root = resource.Site()
+    site = resource.Site()
 
-    root.add_resource(['things'], ActiveThing())
-    root.add_resource(['things'], ThingWrite())
+    site.add_resource(['things'], ActiveThing())
+    site.add_resource(['things'], ThingWrite())
 
-    await aiocoap.Context.create_server_context(root, bind=("0.0.0.0", 1222))
+    await aiocoap.Context.create_server_context(site, bind=("0.0.0.0", 1222))
 
     # Run forever
     await asyncio.get_running_loop().create_future()
